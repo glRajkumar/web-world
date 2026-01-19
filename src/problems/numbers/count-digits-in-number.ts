6c222f173a7c28f965e54782aeaacb850a793668
Raj kumar <glrajv369@gmail.com>
2026-01-19 15:45:06 +0530

    add: numbers/countDigitsInNumber

---
+++ src/problems/numbers/count-digits-in-number.ts
@@ -0,0 +1,15 @@
+
+export function countDigitsInNumber(n: number) {
+  let result = 0
+
+  while (n > 0) {
+    result += 1
+    n = Math.floor(n / 10)
+  }
+
+  return result
+
+  // return Math.floor(Math.log10(n) + 1)
+
+  // return `${n}`.split("").length
+}
